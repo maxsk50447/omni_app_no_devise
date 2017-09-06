7b169ed562c8f82e24082ce6bef950e79a9b2005
Patipol Wangjaitham <patipol.wa@ku.th>
2017-09-07 02:01:33 +0700

    Add routes for sign_in and sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   get  'home/profile', to: 'home#index'
 
+  get  'auth/:provider/callback', to: "sessions#create"
+
+  delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
